5583c9fb47084cd1153be77461285dabf70507fa
Rishabh Ballal <rishabhballal@gmail.com>
2022-11-25 22:33:32 +0100

    mod: toggle

--- static/main.js
+++ static/main.js
@@ -1,19 +1,40 @@
-const cols = document.getElementsByClassName('column');
-Array.from(cols).forEach((col, ncol) => {
-  fetch('static/images/')
-    .then(response => response.text())
-    .then(dir => {
-      const re = new RegExp(`ski_${ncol+1}`, 'g');
-      const nrows = dir.match(re).length/2;
-      for (var i=1; i<=nrows; i++) {
-        var img = document.createElement('img');
-        if (i < 10) {
-          img.src = `static/images/ski_${ncol+1}0${i}.jpg`;
-        } else {
-          img.src = `static/images/ski_${ncol+1}${i}.jpg`;
-        }
-        col.append(img);
+const spos = [
+  [1, 3, 2],
+  [4, 5, 6],
+  [0, 7, 0],
+];
+
+const mpos = [
+  [1, 2, 3],
+];
+
+const cols = document.querySelectorAll('div.column');
+function populate(val) {
+  const pos = (val == 's') ? spos : mpos;
+  Array.from(cols).forEach((col, n) => {
+    col.innerHTML = '';
+    for (var i=0; i<pos.length; i++) {
+      if (!pos[i][n]) continue;
+      var img = document.createElement('img');
+      if (pos[i][n] < 10) {
+        img.src = `static/images/${val}0${pos[i][n]}.jpg`;
+      } else {
+        img.src = `static/images/${val}${pos[i][n]}.jpg`;
       }
-    })
-    .catch(error => console.log(error));
-});
+      col.append(img);
+    };
+  });
+}
+
+var active = document.getElementById('active');
+populate(active.value);
+
+const btns = document.querySelectorAll('button');
+btns.forEach(btn => {
+  btn.addEventListener('click', event => {
+    active.id = '';
+    event.target.id = 'active';
+    active = event.target;
+    populate(active.value);
+  })
+})
